5472494ad5e91ac56c3c1bbeb1a93aa3b419492d
alannguyencs <alannguyen.cs@gmail.com>
2019-09-15 09:25:45 +0800

    refresh master

--- tutorial_01/src/tutorial_01_helloWorld/ShowMessage.java
+++
@@ -1,9 +0,0 @@
-package tutorial_01_helloWorld;
-import javax.swing.JOptionPane;
-
-public class ShowMessage {
-	public static void main(String[] args){
-		JOptionPane.showMessageDialog(null, "Message", "Window Title", 
-		JOptionPane.INFORMATION_MESSAGE);
-	}
-}
