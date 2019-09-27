1fe595df481f5f0656f82ac01741d75f102324f6
alannguyencs <alannguyen.cs@gmail.com>
2019-09-27 13:31:07 +0800

    add tutorial 01

---
+++ tutorial_01/src/tutorial_01_helloWorld/HelloJava.java
@@ -0,0 +1,13 @@
+package tutorial_01_helloWorld;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "CS2360";
+		System.out.print("Hello,");
+		System.out.println(name);
+		System.out.println("How are you today?");
+	}
+
+}
